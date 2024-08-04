6bf75b07cae47792ef40f44979ac27c0114e9c07
Aslam H <iupin5212@gmail.com>
2024-08-05 02:08:40 +0700

    chore: toolbar btn, remove unnecessary class

--- src/components/minimal-tiptap/components/toolbar-button.tsx
+++ src/components/minimal-tiptap/components/toolbar-button.tsx
@@ -1,6 +1,6 @@
+import * as React from 'react'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import { Toggle } from '@/components/ui/toggle'
-import { forwardRef } from 'react'
 import { cn } from '@/lib/utils'
 import type { TooltipContentProps } from '@radix-ui/react-tooltip'
 
@@ -10,39 +10,31 @@ interface ToolbarButtonProps extends React.ComponentPropsWithoutRef<typeof Toggl
   tooltipOptions?: TooltipContentProps
 }
 
-const ToolbarButton = forwardRef<HTMLButtonElement, ToolbarButtonProps>(function ToolbarButton(
-  { isActive, children, tooltip, className, tooltipOptions, ...props },
-  ref
-) {
-  return (
-    <TooltipProvider delayDuration={0}>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Toggle
-            size="sm"
-            ref={ref}
-            className={cn(
-              'rounded disabled:pointer-events-auto disabled:cursor-not-allowed disabled:text-muted-foreground disabled:hover:bg-transparent data-[state=open]:bg-primary/10 data-[state=open]:text-primary',
-              {
-                'bg-primary/10 text-primary hover:bg-primary/10 hover:text-primary': isActive
-              },
-              className
-            )}
-            {...props}
-          >
-            {children}
-          </Toggle>
-        </TooltipTrigger>
-        {tooltip && (
+export const ToolbarButton = React.forwardRef<HTMLButtonElement, ToolbarButtonProps>(
+  ({ isActive, children, tooltip, className, tooltipOptions, ...props }, ref) => {
+    const toggleButton = (
+      <Toggle size="sm" ref={ref} className={cn('size-8 p-0', { 'bg-accent': isActive }, className)} {...props}>
+        {children}
+      </Toggle>
+    )
+
+    if (!tooltip) {
+      return toggleButton
+    }
+
+    return (
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>{toggleButton}</TooltipTrigger>
           <TooltipContent {...tooltipOptions}>
             <div className="flex flex-col items-center text-center">{tooltip}</div>
           </TooltipContent>
-        )}
-      </Tooltip>
-    </TooltipProvider>
-  )
-})
+        </Tooltip>
+      </TooltipProvider>
+    )
+  }
+)
 
 ToolbarButton.displayName = 'ToolbarButton'
 
-export { ToolbarButton }
+export default ToolbarButton
