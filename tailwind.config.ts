60ad89f63a0102aefffc49037fccb44c8716dbb1
Aslam H <iupin5212@gmail.com>
2024-08-05 03:29:07 +0700

    lock file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config = {
   darkMode: ['class'],
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   prefix: '',
+  safelist: ['dark', 'ProseMirror'],
   theme: {
     container: {
       center: true,
