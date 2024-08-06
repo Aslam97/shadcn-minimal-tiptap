4714e32395c8a5544042cf4cbeb8c9e19bd1d4e5
Aslam H <iupin5212@gmail.com>
2024-08-06 15:13:34 +0700

    fix: link btn

--- src/components/minimal-tiptap/components/link/link-popover-block.tsx
+++ src/components/minimal-tiptap/components/link/link-popover-block.tsx
@@ -28,7 +28,7 @@ const LinkPopoverBlock = ({
   return (
     <div className="flex h-10 overflow-hidden rounded bg-background p-2 shadow-lg">
       <div className="inline-flex items-center gap-1">
-        <ToolbarButton tooltip="Edit link" onClick={onEdit}>
+        <ToolbarButton tooltip="Edit link" onClick={onEdit} className="w-auto px-2">
           Edit link
         </ToolbarButton>
         <Separator orientation="vertical" />
