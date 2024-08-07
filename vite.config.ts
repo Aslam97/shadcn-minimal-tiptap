c3535803d9dcab53eb99cf4d252f128ef6d47277
Aslam H <iupin5212@gmail.com>
2024-08-07 19:45:23 +0700

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
