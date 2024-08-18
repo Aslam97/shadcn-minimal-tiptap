f970ea4708de6c1735c8fdd7ef7ef10632148756
Aslam H <iupin5212@gmail.com>
2024-08-18 20:01:57 +0700

    feat(comp): new toolbar section

---
+++ src/components/minimal-tiptap/components/toolbar-section.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react'
+import type { Editor } from '@tiptap/react'
+import { cn } from '@/lib/utils'
+import { CaretDownIcon } from '@radix-ui/react-icons'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { ToolbarButton } from './toolbar-button'
+import { ShortcutKey } from './shortcut-key'
+import { getShortcutKey } from '../utils'
+
+export interface FormatAction {
+  label: string
+  icon?: React.ReactNode
+  action: (editor: Editor) => void
+  isActive: (editor: Editor) => boolean
+  canExecute: (editor: Editor) => boolean
+  shortcuts: string[]
+  value: string
+}
+
+interface ToolbarSectionProps {
+  editor: Editor
+  actions: FormatAction[]
+  activeActions?: string[]
+  mainActionCount?: number
+  dropdownIcon?: React.ReactNode
+  dropdownTooltip?: string
+  dropdownClassName?: string
+}
+
+export const ToolbarSection: React.FC<ToolbarSectionProps> = ({
+  editor,
+  actions,
+  activeActions = actions.map(action => action.value),
+  mainActionCount = 0,
+  dropdownIcon,
+  dropdownTooltip = 'More options',
+  dropdownClassName = 'w-12'
+}) => {
+  const { mainActions, dropdownActions } = React.useMemo(() => {
+    const sortedActions = actions
+      .filter(action => activeActions.includes(action.value))
+      .sort((a, b) => activeActions.indexOf(a.value) - activeActions.indexOf(b.value))
+
+    return {
+      mainActions: sortedActions.slice(0, mainActionCount),
+      dropdownActions: sortedActions.slice(mainActionCount)
+    }
+  }, [actions, activeActions, mainActionCount])
+
+  const renderToolbarButton = React.useCallback(
+    (action: FormatAction) => (
+      <ToolbarButton
+        key={action.label}
+        onClick={() => action.action(editor)}
+        disabled={!action.canExecute(editor)}
+        isActive={action.isActive(editor)}
+        tooltip={`${action.label} ${action.shortcuts.map(s => getShortcutKey(s).symbol).join(' ')}`}
+        aria-label={action.label}
+      >
+        {action.icon}
+      </ToolbarButton>
+    ),
+    [editor]
+  )
+
+  const renderDropdownMenuItem = React.useCallback(
+    (action: FormatAction) => (
+      <DropdownMenuItem
+        key={action.label}
+        onClick={() => action.action(editor)}
+        disabled={!action.canExecute(editor)}
+        className={cn('flex flex-row items-center justify-between gap-4', { 'bg-accent': action.isActive(editor) })}
+        aria-label={action.label}
+      >
+        <span className="grow">{action.label}</span>
+        <ShortcutKey keys={action.shortcuts} />
+      </DropdownMenuItem>
+    ),
+    [editor]
+  )
+
+  const isDropdownActive = React.useMemo(
+    () => dropdownActions.some(action => action.isActive(editor)),
+    [dropdownActions, editor]
+  )
+
+  return (
+    <>
+      {mainActions.map(renderToolbarButton)}
+      {dropdownActions.length > 0 && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <ToolbarButton
+              isActive={isDropdownActive}
+              tooltip={dropdownTooltip}
+              aria-label={dropdownTooltip}
+              className={cn(dropdownClassName)}
+            >
+              {dropdownIcon || <CaretDownIcon className="size-5" />}
+            </ToolbarButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start" className="w-full" onCloseAutoFocus={event => event.preventDefault()}>
+            {dropdownActions.map(renderDropdownMenuItem)}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+    </>
+  )
+}
+
+export default ToolbarSection
