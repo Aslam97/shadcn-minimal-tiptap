99f1c5f39c90cc49b8a09c73dabb2b248493589e
Aslam H <iupin5212@gmail.com>
2024-08-30 04:38:33 +0700

    feat: add number list icon

--- src/components/minimal-tiptap/components/section/four.tsx
+++ src/components/minimal-tiptap/components/section/four.tsx
@@ -13,7 +13,11 @@ const formatActions: ListItem[] = [
   {
     value: 'orderedList',
     label: 'Numbered list',
-    icon: <ListBulletIcon className="size-5" />,
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="currentColor">
+        <path d="M144-144v-48h96v-24h-48v-48h48v-24h-96v-48h120q10.2 0 17.1 6.9 6.9 6.9 6.9 17.1v48q0 10.2-6.9 17.1-6.9 6.9-17.1 6.9 10.2 0 17.1 6.9 6.9 6.9 6.9 17.1v48q0 10.2-6.9 17.1-6.9 6.9-17.1 6.9H144Zm0-240v-96q0-10.2 6.9-17.1 6.9-6.9 17.1-6.9h72v-24h-96v-48h120q10.2 0 17.1 6.9 6.9 6.9 6.9 17.1v72q0 10.2-6.9 17.1-6.9 6.9-17.1 6.9h-72v24h96v48H144Zm48-240v-144h-48v-48h96v192h-48Zm168 384v-72h456v72H360Zm0-204v-72h456v72H360Zm0-204v-72h456v72H360Z" />
+      </svg>
+    ),
     isActive: editor => editor.isActive('orderedList'),
     action: editor => editor.chain().focus().toggleOrderedList().run(),
     canExecute: editor => editor.can().chain().focus().toggleOrderedList().run(),
