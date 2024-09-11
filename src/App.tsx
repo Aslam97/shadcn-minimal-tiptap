7c8dcffe9bdf3ddc759aac780bc18eee1eae67d8
Aslam H <iupin5212@gmail.com>
2024-09-11 07:26:52 +0700

    chore: add example how to expose the editor api and use it through ref

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { z } from 'zod'
 import { cn } from './lib/utils'
 import { Hero } from './components/custom/hero'
 import { BentoMinimalTiptap } from './components/custom/types'
+import { Editor } from '@tiptap/core'
+import { useCallback, useRef } from 'react'
 
 export default function App() {
   return (
@@ -15,30 +17,46 @@ export default function App() {
         <Hero />
         <div className="mt-12 flex flex-col gap-12 sm:mt-20">
           <BentoMinimalTiptap />
+          <ExampleForm />
         </div>
       </main>
     </div>
   )
 }
 
-export const ExampleForm = () => {
-  const formSchema = z.object({
-    description: z
-      .string({
-        required_error: 'Description is required'
-      })
-      .min(1, 'Description is required')
-  })
+const formSchema = z.object({
+  description: z
+    .string({
+      required_error: 'Description is required'
+    })
+    .min(1, 'Description is required')
+})
 
-  const form = useForm<z.infer<typeof formSchema>>({
+type FormValues = z.infer<typeof formSchema>
+
+export const ExampleForm: React.FC = () => {
+  const editorRef = useRef<Editor | null>(null)
+  const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       description: ''
     }
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const handleCreate = useCallback(
+    ({ editor }: { editor: Editor }) => {
+      if (form.getValues('description') && editor.isEmpty) {
+        editor.commands.setContent(form.getValues('description'))
+      }
+      editorRef.current = editor
+    },
+    [form]
+  )
+
+  const onSubmit = (values: FormValues) => {
     console.log(values)
+    form.reset()
+    editorRef.current?.commands.clearContent()
   }
 
   return (
@@ -60,6 +78,7 @@ export const ExampleForm = () => {
                   editorContentClassName="some-class"
                   output="html"
                   placeholder="Type your description here..."
+                  onCreate={handleCreate}
                   autofocus={true}
                   immediatelyRender={true}
                   editable={true}
