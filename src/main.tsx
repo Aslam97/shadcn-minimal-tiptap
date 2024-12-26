6cc0b1b2cd5fcb76dda7e08863c00f4ea64c99fe
Aslam H <iupin5212@gmail.com>
2024-12-26 14:18:09 +0700

    chore: meta tag

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,14 @@ import './global.css'
 import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from 'next-themes'
 import { TooltipProvider } from '@/components/ui/tooltip'
-import { HelmetProvider } from 'react-helmet-async'
-import { SEO } from './components/custom/seo.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <TooltipProvider delayDuration={0}>
-        <HelmetProvider>
-          <App />
-          <SEO />
-          <Toaster />
-        </HelmetProvider>
+        <App />
+        <Toaster />
       </TooltipProvider>
       <Analytics />
     </ThemeProvider>
